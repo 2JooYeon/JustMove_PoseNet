378a64798f2f174bd54c5e56863ca2d1bf420ee9
2JooYeon <lewndus7749@naver.com>
2021-02-17 05:52:13 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// Steps to apply posenet to react webcam
+// 1. Install dependencies OK
+// 2. Import dependencies OK
+// 3. Setup webcam and canvas OK
+// 4. Define references to those
+// 5. Load posenet
+// 6. Detect function
+// 7. Drawing utilities from tensorflow
+// 8. Draw functions
+
+import React, { useRef } from 'react';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as posenet from '@tensorflow-models/posenet';
+import Webcam from 'react-webcam';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello world!</header>
     </div>
   );
 }
