553ae09252b6884b7d1b6aec31906729d2c6a343
2JooYeon <lewndus7749@naver.com>
2021-02-17 06:43:05 +0900

    Load Posenet & Detect keypoints & Draw skeleton line using drawing utilities from tf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 // Steps to apply posenet to react webcam
+
 // 1. Install dependencies OK
 // 2. Import dependencies OK
 // 3. Setup webcam and canvas OK
-// 4. Define references to those
-// 5. Load posenet
-// 6. Detect function
-// 7. Drawing utilities from tensorflow
-// 8. Draw functions
+// 4. Define references to those OK
+// 5. Load posenet OK
+// 6. Detect function OK
+// 7. Drawing utilities from tensorflow OK
+// 8. Draw functions OK
 
 import React, { useRef } from 'react';
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import * as posenet from '@tensorflow-models/posenet';
 import Webcam from 'react-webcam';
+import { drawKeypoints, drawSkeleton } from './utilities';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.5,
+    });
+
+    setInterval(() => {
+      detect(net);
+    }, 16.7);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      // drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+      requestAnimationFrame(()=>{drawCanvas(pose, video, videoWidth, videoHeight, canvasRef)});
+    }
+  };
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext('2d');
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose['keypoints'], 0.6, ctx);
+    drawSkeleton(pose['keypoints'], 0.7, ctx);
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
+          ref={webcamRef}
           style={{
             position: 'absolute',
             marginLeft: 'auto',
@@ -33,6 +85,7 @@ function App() {
         />
 
         <canvas
+          ref={canvasRef}
           style={{
             position: 'absolute',
             marginLeft: 'auto',
